3210480d91a4d1c501900d0fd4f3e656bfb98d68
shreyas_kawale <shreyas.kawale@noovosoft.com>
2024-05-27 17:22:29 +0530

    write serializers for publisher and reviews

--- book/serializers.py
+++ book/serializers.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
-from .models import Book, Author
+from .models import Book, Author, Publisher, Reviews
 
 
-class BookSerializer(serializers.ModelSerializer):
-    author_name = serializers.SlugRelatedField(slug_field='name', queryset=Author.objects.all())
-
+class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book
-        fields = ['title', 'author_name', 'genre', 'price']
+        model = Publisher
+        fields = ['name']
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['name', 'age', 'nationality']
+        fields = ['name', 'age', 'date_of_birth', 'date_of_death', 'nationality', 'photo']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    publisher = PublisherSerializer(read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'genre', 'publication_date', 'publisher', 'rating', 'price']
+
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Reviews
+        fields = ['content', 'book']
